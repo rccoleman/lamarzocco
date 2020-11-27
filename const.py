5c67d036ea04b9d6fd2fe284c8f5598c8da4dc1c
Rob Coleman <rccoleman@gmail.com>
2020-11-27 15:48:44 -0800

    Fixed names of attributes

--- const.py
+++ const.py
@@ -6,7 +6,7 @@ CONF_SERIAL_NUMBER = "serial_number"
 CONF_CLIENT_ID = "client_id"
 CONF_CLIENT_SECRET = "client_secret"
 
-DEFAULT_NAME = "lamarzocco"
+DEFAULT_NAME = "la_marzocco"
 
 ATTR_ENABLE_PREBREWING = "ENABLE_PREBREWING"
 ATTR_STBY_TIMER = "STBY_TIMER"
@@ -40,23 +40,23 @@ COMMAND_STANDBY = {"status": "STANDBY"}
 GW_URL = "https://gw.lamarzocco.io/v1/home/machines"
 
 ATTR_MAP = {
-    STATE_STATUS: "Status",
-    ATTR_ENABLE_PREBREWING: "Prebrewing Enabled",
-    ATTR_STBY_TIMER: "Standby Timer",
-    ATTR_TON_PREBREWING_K1: "Prebrewing Time On (Key 1)",
-    ATTR_TON_PREBREWING_K2: "Prebrewing Time On (Key 2)",
-    ATTR_TON_PREBREWING_K3: "Prebrewing Time On (Key 3)",
-    ATTR_TON_PREBREWING_K4: "Prebrewing Time On (Key 4)",
-    ATTR_TOFF_PREBREWING_K1: "Prebrewing Time Off (Key 1)",
-    ATTR_TOFF_PREBREWING_K2: "Prebrewing Time Off (Key 2)",
-    ATTR_TOFF_PREBREWING_K3: "Prebrewing Time Off (Key 3)",
-    ATTR_TOFF_PREBREWING_K4: "Prebrewing Time Off (Key 4)",
-    ATTR_TSET_STEAM: "Steam Temperature",
-    ATTR_TSET_COFFEE: "Coffee Temperature",
-    ATTR_DOSE_K1: "Dose (Key 1)",
-    ATTR_DOSE_K2: "Dose (Key 2)",
-    ATTR_DOSE_K3: "Dose (Key 3)",
-    ATTR_DOSE_K4: "Dose (Key 4)",
-    ATTR_DOSE_K5: "Dose (Key 5)",
-    ATTR_DOSE_TEA: "Dose (Tea)",
+    STATE_STATUS: "status",
+    ATTR_ENABLE_PREBREWING: "prebrewing_enabled",
+    ATTR_STBY_TIMER: "standby_timer",
+    ATTR_TON_PREBREWING_K1: "prebrewing_ton_k1",
+    ATTR_TON_PREBREWING_K2: "prebrewing_ton_k2",
+    ATTR_TON_PREBREWING_K3: "prebrewing_ton_k3",
+    ATTR_TON_PREBREWING_K4: "prebrewing_ton_k4",
+    ATTR_TOFF_PREBREWING_K1: "prebrewing_toff_k1",
+    ATTR_TOFF_PREBREWING_K2: "prebrewing_toff_k2",
+    ATTR_TOFF_PREBREWING_K3: "prebrewing_toff_k3",
+    ATTR_TOFF_PREBREWING_K4: "prebrewing_toff_k4",
+    ATTR_TSET_STEAM: "steam_temperature",
+    ATTR_TSET_COFFEE: "coffee_temperature",
+    ATTR_DOSE_K1: "dose_k1",
+    ATTR_DOSE_K2: "dose_k2",
+    ATTR_DOSE_K3: "dose_k3",
+    ATTR_DOSE_K4: "dose_k4",
+    ATTR_DOSE_K5: "dose_k5",
+    ATTR_DOSE_TEA: "dose_tea",
 }

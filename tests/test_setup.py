75368a15402d6e193cf0fb862becf5e84645dc20
Rob Coleman <rccoleman@gmail.com>
2021-01-03 14:47:32 -0800

    Fix and improve this broken test

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,4 +1,5 @@
-"""Test La Marzocco machine"""
+"""Test La Marzocco machine setup."""
+import logging
 from copy import deepcopy
 
 import lmdirect
@@ -16,6 +17,10 @@ from custom_components.lamarzocco.const import (
     DOMAIN,
 )
 
+ENTRY_ID = 1
+
+_LOGGER = logging.getLogger(__name__)
+
 DATA = {
     CONF_HOST: "1.2.3.4",
     CONF_CLIENT_ID: "aabbcc",
@@ -23,25 +28,25 @@ DATA = {
     CONF_USERNAME: "username",
     CONF_PASSWORD: "password",
     CONF_SERIAL_NUMBER: "aabbcc",
-    CONF_MODEL_NAME: "aaaaa",
+    CONF_MODEL_NAME: "GS3 AV",
     CONF_MACHINE_NAME: "bbbbb",
 }
 
 
-async def setup_lm_machine(hass, config=DATA):
+async def setup_lm_machine(hass):
+    """Set up a test configuration."""
+
     await async_setup_component(hass, DOMAIN, {})
 
     config_entry = MockConfigEntry(
         domain=DOMAIN,
-        data=deepcopy(config),
-        entry_id=1,
+        data=deepcopy(DATA),
+        entry_id=ENTRY_ID,
     )
 
     config_entry.add_to_hass(hass)
-
     await hass.config_entries.async_setup(config_entry.entry_id)
     await hass.async_block_till_done()
-
     assert config_entry.entry_id in hass.data[DOMAIN]
 
     machine = hass.data[DOMAIN][config_entry.entry_id]
@@ -50,30 +55,19 @@ async def setup_lm_machine(hass, config=DATA):
     return machine
 
 
-@patch("custom_components.lamarzocco.api.LaMarzocco.connect")
+@patch.object(lmdirect.LMDirect, "_connect", autospec=True)
 @patch.object(lmdirect.LMDirect, "_send_msg", autospec=True)
 async def test_setup_lm_machine(
     mock_send_msg,
     mock_connect,
     hass,
-    config=DATA,
 ):
-    mock_connect.return_value = DATA
-    lm = await setup_lm_machine(hass, config)
-    assert lm.machine_name == "bbbbb"
-
-
-@patch("custom_components.lamarzocco.api.LaMarzocco.connect")
-@patch.object(lmdirect.LMDirect, "_send_msg", autospec=True)
-async def test_controller_unload(
-    mock_send_msg,
-    mock_connect,
-    hass,
-):
-    entry_id = 1
-    mock_connect.return_value = DATA
+    """Test set up and tear down."""
     machine = await setup_lm_machine(hass)
-    assert machine == hass.data[DOMAIN][entry_id]
+    assert machine == hass.data[DOMAIN][ENTRY_ID]
 
-    assert await hass.config_entries.async_unload(entry_id)
+    config_entry = hass.config_entries.async_get_entry(ENTRY_ID)
+    assert config_entry.data == DATA
+
+    assert await hass.config_entries.async_unload(ENTRY_ID)
     assert not hass.data[DOMAIN]

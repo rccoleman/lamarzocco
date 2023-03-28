ce05d4b0b3f6b79c1c75f0ed1bd080329abe37a2
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2023-03-28 11:07:00 +0200

    model more interface methods

--- custom_components/lamarzocco/interface.py
+++ custom_components/lamarzocco/interface.py
@@ -2,41 +2,101 @@
 from LMDirect import LMDirect
 from LMCloud import LMCloud
 
+from const import *
+
 '''
 Class to interface between lmdirect and lmcloud
 '''
 class LMInterface:
 
-@property
-def model_name(self):
-    return self._model_name
+    @property
+    def model_name(self):
+        return self._model_name
 
-lm_direct = None
-lm_cloud = None
+    lm_direct = None
+    lm_cloud = None
 
-'''
-Get model name during initialization
-'''
-def __init__(self, creds):
-    pass
+ 
+    def __init__(self, config):
+        self.lm_cloud = LMCloud.create(config)
+        self.model_name = self.lm_cloud.model_name
 
-async def connect():
-    if self.model_name == MODEL_LMU:
-        pass
-    else:
-        lm_direct.connect()
-
-async def request_status():
-    if self.model_name == MODEL_LMU:
-        lm_cloud.get_status()
-    else:
-        lm_direct.request_status()
-
-async def set_auto_on_off_times(self, day_of_week, hour_on, minute_on, hour_off, minute_off):
-    if self.model_name == MODEL_LMU:
-        lm_cloud.set_auto_on_off(day_of_week, hour_on, minute_on, hour_off, minute_off)
-    else:
-        lm_direct.set_auto_on_off_times(day_of_week, hour_on, minute_on, hour_off, minute_off)
+        if self.model_name in LM_CLOUD_MODELS:
+            self.lm_cloud = LMCloud.create(config, config["ip"], config["port"])
+        else:
+            self.lm_direct = LMDirect.__init__(config)
 
 
+    async def init_data(self, hass):
+        if self.model_name in LM_CLOUD_MODELS:
+            pass
+        else:
+            """Initialize the underlying lmdirect package."""
 
+            """Register the callback to receive updates."""
+            self.register_callback(self.update_callback)
+
+            self._run = True
+
+            """Start polling for status."""
+            self._polling_task = hass.loop.create_task(
+                self.fetch_data(), name="Polling Loop"
+            )
+
+            """Reap the results and any any exceptions."""
+            self._poll_reaper_task = hass.loop.create_task(
+                self.poll_reaper(), name="Poll Reaper"
+            )
+
+
+    '''
+    interface methods
+    '''
+    async def connect(self):
+        if self.model_name in LM_CLOUD_MODELS:
+            pass
+        else:
+            self.lm_direct.connect()
+
+
+    async def request_status(self):
+        if self.model_name in LM_CLOUD_MODELS:
+            self.lm_cloud.get_status()
+        else:
+            self.lm_direct.request_status()
+
+
+    async def set_auto_on_off_enable(self, day_of_week, enable):
+        if self.model_name in LM_CLOUD_MODELS:
+            schedule = self.lm_cloud.get_schedule()
+            idx = [index for (index, d) in enumerate(schedule) if d["day"] == day_of_week.upper()][0]
+            schedule[idx]["enable"] = True
+            self.lm_cloud.configure_schedule(True, schedule)
+        else:
+            self.lm_direct.set_auto_on_off_enable(day_of_week, enable)
+
+
+    async def set_auto_on_off_times(self, day_of_week, hour_on, minute_on, hour_off, minute_off):
+        if self.model_name in LM_CLOUD_MODELS:
+            self.lm_cloud.set_auto_on_off(day_of_week, hour_on, minute_on, hour_off, minute_off)
+        else:
+            self.lm_direct.set_auto_on_off_times(day_of_week, hour_on, minute_on, hour_off, minute_off)
+
+
+    async def set_dose(self, key, pulses):
+        if self.model_name in LM_CLOUD_MODELS:
+            pass
+        else:
+            self.lm_direct.set_dose(key, pulses)
+
+    async def set_prebrew_times(self, key, seconds_on, seconds_off):
+        if self.model_name in LM_CLOUD_MODELS:
+            self.lm_cloud.configure_prebrew(prebrewOnTime=seconds_on, prebrewOffTime=seconds_off)
+        else:
+            self.lm_direct.set_prebrew_times(key, seconds_on, seconds_off)
+
+    async def set_preinfusion_time(self, key, seconds):
+        if self.model_name in LM_CLOUD_MODELS:
+            pass
+        else:
+            self.lm_direct.set_preinfusion_time(key, seconds)
